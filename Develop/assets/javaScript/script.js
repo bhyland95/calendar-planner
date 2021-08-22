a34542c860744174f6f0847b34ba3d842e1afbad
BH <blakehyland95@gmail.com>
2021-08-22 00:08:35 -0600

    Added function to check current hour against schedule blocks and then styles accordingly

--- Develop/assets/javaScript/script.js
+++ Develop/assets/javaScript/script.js
@@ -13,14 +13,48 @@ $(".saveBtn").on("click", function () {
     localStorage.setItem(timeSlot, text);
 });
 
+
+//FUNCTION TO CHECK CURRENT HOUR AGAINST SCHEDULE
+function currentHour() {
+    //get current hour
+    var timeNow = parseInt(moment().format('HH'));
+    console.log(timeNow)
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var hourBlock = parseInt($(this).attr("id"));
+
+        //if current hour is greater than HourBlock then adds class of past 
+        if (timeNow > hourBlock) {
+            $(this).addClass("past")
+            $(this).removeClass("present")
+            $(this).removeClass("future")
+        //if current hour is equal to current HourBlock add class of present 
+        } else if (timeNow === hourBlock) {
+            $(this).addClass("present")
+            $(this).removeClass("past")
+            $(this).removeClass("future")
+        //if current hour is less than current HourBlock add class of future
+        } else {
+            $(this).addClass("future")
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+        }
+    })
+}
+
 // Gets timeSlot text from localStorage if there is one.
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+
+//Run currenthour function
+currentHour();
