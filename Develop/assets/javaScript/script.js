5d5e9a357b94b378fe2843ff59481426ef0e5bb7
bhyland <blakehyland95@gmail.com>
2021-08-21 22:00:17 -0600

    added localstorage functionality when save button is clicked

--- Develop/assets/javaScript/script.js
+++ Develop/assets/javaScript/script.js
@@ -1,5 +1,26 @@
 var currentTime = moment()
+var tasksArr = [];
 
 
 //Displays current day at top of scheduler
 $("#currentDay").text(currentTime.format('LL'))
+
+//Saves Hour Slots to localStorage if savebutton is clicked
+$(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var timeSlot = $(this).parent().attr('id');
+
+    localStorage.setItem(timeSlot, text);
+});
+
+// Gets timeSlot text from localStorage if there is one.
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
